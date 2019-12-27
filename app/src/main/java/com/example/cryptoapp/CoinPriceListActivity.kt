955ac3f9465e06384a4aef4d45b940fe01543e02
suminfx <sumin931@yandex.ru>
2019-12-27 15:35:19 +0300

    Экран с детальной информацией готов

--- app/src/main/java/com/example/cryptoapp/CoinPriceListActivity.kt
+++ app/src/main/java/com/example/cryptoapp/CoinPriceListActivity.kt
@@ -1,7 +1,6 @@
 package com.example.cryptoapp
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -19,7 +18,11 @@ class CoinPriceListActivity : AppCompatActivity() {
         val adapter = CoinInfoAdapter(this)
         adapter.onCoinClickListener = object : CoinInfoAdapter.OnCoinClickListener {
             override fun onCoinClick(coinPriceInfo: CoinPriceInfo) {
-                Log.d("ON_CLICK_TEST", coinPriceInfo.fromSymbol)
+                val intent = CoinDetailActivity.newIntent(
+                    this@CoinPriceListActivity,
+                    coinPriceInfo.fromSymbol
+                )
+                startActivity(intent)
             }
         }
         rvCoinPriceList.adapter = adapter
