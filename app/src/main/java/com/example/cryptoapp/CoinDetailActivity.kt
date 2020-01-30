9cbf3f56d7c01c36a61b7454830d0140fbc9a92a
a.sumin <a.sumin@cardsmobile.ru>
2020-01-30 16:44:51 +0300

    Refactoring

--- app/src/main/java/com/example/cryptoapp/CoinDetailActivity.kt
+++ app/src/main/java/com/example/cryptoapp/CoinDetailActivity.kt
@@ -2,12 +2,10 @@ package com.example.cryptoapp
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_coin_detail.*
 
