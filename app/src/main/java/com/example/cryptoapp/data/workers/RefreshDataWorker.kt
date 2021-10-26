f8131542d6e92fde683eb5299332d1656467b437
a.sumin <a.sumin@cardsmobile.ru>
2021-10-27 01:14:14 +0300

    Use RefreshDataWorker.kt

---
+++ app/src/main/java/com/example/cryptoapp/data/workers/RefreshDataWorker.kt
@@ -0,0 +1,46 @@
+package com.example.cryptoapp.data.workers
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.OneTimeWorkRequest
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkerParameters
+import com.example.cryptoapp.data.database.AppDatabase
+import com.example.cryptoapp.data.mapper.CoinMapper
+import com.example.cryptoapp.data.network.ApiFactory
+import kotlinx.coroutines.delay
+
+class RefreshDataWorker(
+    context: Context,
+    workerParameters: WorkerParameters
+) : CoroutineWorker(context, workerParameters) {
+
+    private val coinInfoDao = AppDatabase.getInstance(context).coinPriceInfoDao()
+    private val apiService = ApiFactory.apiService
+
+    private val mapper = CoinMapper()
+
+    override suspend fun doWork(): Result {
+        while (true) {
+            try {
+                val topCoins = apiService.getTopCoinsInfo(limit = 50)
+                val fSyms = mapper.mapNamesListToString(topCoins)
+                val jsonContainer = apiService.getFullPriceList(fSyms = fSyms)
+                val coinInfoDtoList = mapper.mapJsonContainerToListCoinInfo(jsonContainer)
+                val dbModelList = coinInfoDtoList.map { mapper.mapDtoToDbModel(it) }
+                coinInfoDao.insertPriceList(dbModelList)
+            } catch (e: Exception) {
+            }
+            delay(10000)
+        }
+    }
+
+    companion object {
+
+        const val NAME = "RefreshDataWorker"
+
+        fun makeRequest(): OneTimeWorkRequest {
+            return OneTimeWorkRequestBuilder<RefreshDataWorker>().build()
+        }
+    }
+}
--- app/src/main/java/com/example/cryptoapp/presentation/CoinViewModel.kt
+++ app/src/main/java/com/example/cryptoapp/presentation/CoinViewModel.kt
@@ -2,12 +2,10 @@ package com.example.cryptoapp.presentation
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.viewModelScope
 import com.example.cryptoapp.data.repository.CoinRepositoryImpl
 import com.example.cryptoapp.domain.GetCoinInfoListUseCase
 import com.example.cryptoapp.domain.GetCoinInfoUseCase
 import com.example.cryptoapp.domain.LoadDataUseCase
-import kotlinx.coroutines.launch
 
 class CoinViewModel(application: Application) : AndroidViewModel(application) {
 
@@ -22,8 +20,6 @@ class CoinViewModel(application: Application) : AndroidViewModel(application) {
     fun getDetailInfo(fSym: String) = getCoinInfoUseCase(fSym)
 
     init {
-        viewModelScope.launch {
-            loadDataUseCase()
-        }
+        loadDataUseCase()
     }
 }
