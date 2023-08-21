2b74a1deb56f833efbf18acfcc8222a0c07f458d
Vitaly <onebeattrue@gmail.com>
2023-08-22 02:33:01 +0300

    Оптимизация Recycler

--- app/src/main/java/com/example/cryptoapp/presentation/adapters/CoinInfoAdapter.kt
+++ app/src/main/java/com/example/cryptoapp/presentation/adapters/CoinInfoAdapter.kt
@@ -3,20 +3,14 @@ package com.example.cryptoapp.presentation.adapters
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.ListAdapter
 import com.example.cryptoapp.R
 import com.example.cryptoapp.databinding.ItemCoinInfoBinding
 import com.example.cryptoapp.domain.CoinInfo
 import com.squareup.picasso.Picasso
 
 class CoinInfoAdapter(private val context: Context) :
-    RecyclerView.Adapter<CoinInfoViewHolder>() {
-
-    var coinInfoList: List<CoinInfo> = listOf()
-        set(value) {
-            field = value
-            notifyDataSetChanged()
-        }
+    ListAdapter<CoinInfo, CoinInfoViewHolder>(CoinInfoDiffCallback) {
 
     var onCoinClickListener: OnCoinClickListener? = null
 
@@ -25,18 +19,15 @@ class CoinInfoAdapter(private val context: Context) :
         return CoinInfoViewHolder(binding)
     }
 
-    override fun getItemCount() = coinInfoList.size
-
     override fun onBindViewHolder(holder: CoinInfoViewHolder, position: Int) {
-        val coin = coinInfoList[position]
+        val coin = getItem(position)
         with(holder.binding) {
             with(coin) {
                 val symbolsTemplate = context.resources.getString(R.string.symbols_template)
                 val lastUpdateTemplate = context.resources.getString(R.string.last_update_template)
                 tvSymbols.text = String.format(symbolsTemplate, fromSymbol, toSymbol)
                 tvPrice.text = price
-                tvLastUpdate.text =
-                    String.format(lastUpdateTemplate, lastUpdate)
+                tvLastUpdate.text = String.format(lastUpdateTemplate, lastUpdate)
                 Picasso.get().load(imageUrl).into(ivLogoCoin)
                 root.setOnClickListener {
                     onCoinClickListener?.onCoinClick(this)
---
+++ app/src/main/java/com/example/cryptoapp/presentation/adapters/CoinInfoDiffCallback.kt
@@ -0,0 +1,14 @@
+package com.example.cryptoapp.presentation.adapters
+
+import androidx.recyclerview.widget.DiffUtil
+import com.example.cryptoapp.domain.CoinInfo
+
+object CoinInfoDiffCallback : DiffUtil.ItemCallback<CoinInfo>() {
+    override fun areItemsTheSame(oldItem: CoinInfo, newItem: CoinInfo): Boolean {
+        return oldItem.fromSymbol == newItem.fromSymbol
+    }
+
+    override fun areContentsTheSame(oldItem: CoinInfo, newItem: CoinInfo): Boolean {
+        return oldItem == newItem
+    }
+}
