0e10ee47fb0e831e7cb7bfc2049b2e5aa9cab306
sumin93 <suminfx@mail.ru>
2024-04-10 13:13:31 +0300

    Use CoinDetailFragment in CoinDetailActivity

--- app/src/main/java/com/example/cryptoapp/presentation/CoinDetailActivity.kt
+++ app/src/main/java/com/example/cryptoapp/presentation/CoinDetailActivity.kt
@@ -3,17 +3,11 @@ package com.example.cryptoapp.presentation
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
 import com.example.cryptoapp.R
 import com.example.cryptoapp.databinding.ActivityCoinDetailBinding
-import com.squareup.picasso.Picasso
 
 class CoinDetailActivity : AppCompatActivity() {
-
-    private lateinit var viewModel: CoinViewModel
 
     private val binding by lazy {
         ActivityCoinDetailBinding.inflate(layoutInflater)
@@ -27,28 +21,11 @@ class CoinDetailActivity : AppCompatActivity() {
             return
         }
         val fromSymbol = intent.getStringExtra(EXTRA_FROM_SYMBOL) ?: EMPTY_SYMBOL
-        viewModel = ViewModelProvider(this)[CoinViewModel::class.java]
-
-        val tvPrice = findViewById<TextView>(R.id.tvPrice)
-        val tvMinPrice = findViewById<TextView>(R.id.tvMinPrice)
-        val tvMaxPrice = findViewById<TextView>(R.id.tvMaxPrice)
-        val tvLastMarket = findViewById<TextView>(R.id.tvLastMarket)
-        val tvLastUpdate = findViewById<TextView>(R.id.tvLastUpdate)
-        val tvFromSymbol = findViewById<TextView>(R.id.tvFromSymbol)
-        val tvToSymbol = findViewById<TextView>(R.id.tvToSymbol)
-        val ivLogoCoin = findViewById<ImageView>(R.id.ivLogoCoin)
-
-        viewModel.getDetailInfo(fromSymbol).observe(this) {
-            with(binding) {
-                tvPrice.text = it.price
-                tvMinPrice.text = it.lowDay
-                tvMaxPrice.text = it.highDay
-                tvLastMarket.text = it.lastMarket
-                tvLastUpdate.text = it.lastUpdate
-                tvFromSymbol.text = it.fromSymbol
-                tvToSymbol.text = it.toSymbol
-                Picasso.get().load(it.imageUrl).into(ivLogoCoin)
-            }
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_container, CoinDetailFragment.newInstance(fromSymbol))
+                .commit()
         }
     }
 
