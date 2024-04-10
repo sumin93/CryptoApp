29d7ba2b044d42926a6210e017928a822f100661
sumin93 <suminfx@mail.ru>
2024-04-10 11:49:03 +0300

    task2

--- app/src/main/java/com/example/cryptoapp/database/CoinPriceInfoDao.kt
+++ app/src/main/java/com/example/cryptoapp/database/CoinPriceInfoDao.kt
@@ -9,7 +9,7 @@ import com.example.cryptoapp.pojo.CoinPriceInfo
 
 @Dao
 interface CoinPriceInfoDao {
-    @Query("SELECT * FROM full_price_list ORDER BY lastUpdate DESC")
+    @Query("SELECT * FROM full_price_list ORDER BY lastUpdate DESC LIMIT 0")
     fun getPriceList(): LiveData<List<CoinPriceInfo>>
 
     @Query("SELECT * FROM full_price_list WHERE fromSymbol == :fSym LIMIT 1")
