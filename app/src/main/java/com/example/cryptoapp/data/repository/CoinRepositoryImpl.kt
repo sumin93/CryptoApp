c35839669eeac0ae22c0bc69d1363a44826f3c06
sumin93 <suminfx@mail.ru>
2024-04-10 13:09:30 +0300

    Using viewBinding

--- app/src/main/java/com/example/cryptoapp/data/repository/CoinRepositoryImpl.kt
+++ app/src/main/java/com/example/cryptoapp/data/repository/CoinRepositoryImpl.kt
@@ -3,23 +3,23 @@ package com.example.cryptoapp.data.repository
 import android.app.Application
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.map
-import androidx.work.ExistingWorkPolicy
-import androidx.work.WorkManager
 import com.example.cryptoapp.data.database.AppDatabase
 import com.example.cryptoapp.data.mapper.CoinMapper
-import com.example.cryptoapp.data.workers.RefreshDataWorker
+import com.example.cryptoapp.data.network.ApiFactory
 import com.example.cryptoapp.domain.CoinInfo
 import com.example.cryptoapp.domain.CoinRepository
+import kotlinx.coroutines.delay
 
 class CoinRepositoryImpl(
     private val application: Application
 ) : CoinRepository {
 
     private val coinInfoDao = AppDatabase.getInstance(application).coinPriceInfoDao()
+    private val apiService = ApiFactory.apiService
+
     private val mapper = CoinMapper()
 
     override fun getCoinInfoList(): LiveData<List<CoinInfo>> {
-        // Тут ранее было Transformations.map, сейчас заменили на coinInfoDao.getPriceList().map
         return coinInfoDao.getPriceList().map {
             it.map {
                 mapper.mapDbModelToEntity(it)
@@ -28,18 +28,23 @@ class CoinRepositoryImpl(
     }
 
     override fun getCoinInfo(fromSymbol: String): LiveData<CoinInfo> {
-        // Тут ранее было Transformations.map, сейчас заменили на coinInfoDao.getPriceInfoAboutCoin(fromSymbol).map
         return coinInfoDao.getPriceInfoAboutCoin(fromSymbol).map {
             mapper.mapDbModelToEntity(it)
         }
     }
 
-    override fun loadData() {
-        val workManager = WorkManager.getInstance(application)
-        workManager.enqueueUniqueWork(
-            RefreshDataWorker.NAME,
-            ExistingWorkPolicy.REPLACE,
-            RefreshDataWorker.makeRequest()
-        )
+    override suspend fun loadData() {
+        while (true) {
+            try {
+                val topCoins = apiService.getTopCoinsInfo(limit = 50)
+                val fSyms = mapper.mapNamesListToString(topCoins)
+                val jsonContainer = apiService.getFullPriceList(fSyms = fSyms)
+                val coinInfoDtoList = mapper.mapJsonContainerToListCoinInfo(jsonContainer)
+                val dbModelList = coinInfoDtoList.map { mapper.mapDtoToDbModel(it) }
+                coinInfoDao.insertPriceList(dbModelList)
+            } catch (e: Exception) {
+            }
+            delay(10000)
+        }
     }
 }
--- app/src/main/java/com/example/cryptoapp/presentation/CoinPriceListActivity.kt
+++ app/src/main/java/com/example/cryptoapp/presentation/CoinPriceListActivity.kt
@@ -3,7 +3,6 @@ package com.example.cryptoapp.presentation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import com.example.cryptoapp.R
 import com.example.cryptoapp.databinding.ActivityCoinPriceListBinding
 import com.example.cryptoapp.domain.CoinInfo
 import com.example.cryptoapp.presentation.adapters.CoinInfoAdapter
@@ -22,37 +21,17 @@ class CoinPriceListActivity : AppCompatActivity() {
         val adapter = CoinInfoAdapter(this)
         adapter.onCoinClickListener = object : CoinInfoAdapter.OnCoinClickListener {
             override fun onCoinClick(coinPriceInfo: CoinInfo) {
-                if (isOnePaneMode()) {
-                    launchDetailActivity(coinPriceInfo.fromSymbol)
-                } else {
-                    launchDetailFragment(coinPriceInfo.fromSymbol)
-                }
+                val intent = CoinDetailActivity.newIntent(
+                    this@CoinPriceListActivity,
+                    coinPriceInfo.fromSymbol
+                )
+                startActivity(intent)
             }
         }
         binding.rvCoinPriceList.adapter = adapter
-        binding.rvCoinPriceList.itemAnimator = null
         viewModel = ViewModelProvider(this)[CoinViewModel::class.java]
         viewModel.coinInfoList.observe(this) {
-            adapter.submitList(it)
+            adapter.coinInfoList = it
         }
-    }
-
-    private fun isOnePaneMode() = binding.fragmentContainer == null
-
-    private fun launchDetailActivity(fromSymbol: String) {
-        val intent = CoinDetailActivity.newIntent(
-            this@CoinPriceListActivity,
-            fromSymbol
-        )
-        startActivity(intent)
-    }
-
-    private fun launchDetailFragment(fromSymbol: String) {
-        supportFragmentManager.popBackStack()
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.fragment_container, CoinDetailFragment.newInstance(fromSymbol))
-            .addToBackStack(null)
-            .commit()
     }
 }
