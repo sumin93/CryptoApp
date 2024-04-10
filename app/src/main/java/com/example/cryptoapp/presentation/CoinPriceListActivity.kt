1faff9040cbf45111fd61c2102e52f540c77ab09
sumin93 <suminfx@mail.ru>
2024-04-10 13:09:30 +0300

    Using viewBinding

--- app/src/main/java/com/example/cryptoapp/presentation/CoinPriceListActivity.kt
+++ app/src/main/java/com/example/cryptoapp/presentation/CoinPriceListActivity.kt
@@ -29,10 +29,9 @@ class CoinPriceListActivity : AppCompatActivity() {
             }
         }
         binding.rvCoinPriceList.adapter = adapter
-        binding.rvCoinPriceList.itemAnimator = null
         viewModel = ViewModelProvider(this)[CoinViewModel::class.java]
         viewModel.coinInfoList.observe(this) {
-            adapter.submitList(it)
+            adapter.coinInfoList = it
         }
     }
 }
