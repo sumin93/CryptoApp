bd22231e23b30a8f660e3e128393bbed00c0710d
sumin93 <suminfx@mail.ru>
2025-02-18 16:00:29 +0300

    Upgrade dependencies

--- app/src/main/java/com/example/cryptoapp/CoinPriceListActivity.kt
+++ app/src/main/java/com/example/cryptoapp/CoinPriceListActivity.kt
@@ -3,10 +3,10 @@ package com.example.cryptoapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.example.cryptoapp.adapters.CoinInfoAdapter
 import com.example.cryptoapp.pojo.CoinPriceInfo
-import kotlinx.android.synthetic.main.activity_coin_prce_list.*
 
 class CoinPriceListActivity : AppCompatActivity() {
 
@@ -25,8 +25,9 @@ class CoinPriceListActivity : AppCompatActivity() {
                 startActivity(intent)
             }
         }
+        val rvCoinPriceList = findViewById<RecyclerView>(R.id.rvCoinPriceList)
         rvCoinPriceList.adapter = adapter
-        viewModel = ViewModelProviders.of(this)[CoinViewModel::class.java]
+        viewModel = ViewModelProvider(this)[CoinViewModel::class.java]
         viewModel.priceList.observe(this, Observer {
             adapter.coinInfoList = it
         })
