dc037ef7eb5bcc7d40a7a843ae0ef5225c85b123
sumin93 <suminfx@mail.ru>
2025-06-06 20:57:57 +0300

    Upgrade dependencies

--- app/src/main/java/com/example/cryptoapp/data/repository/CoinRepositoryImpl.kt
+++ app/src/main/java/com/example/cryptoapp/data/repository/CoinRepositoryImpl.kt
@@ -2,7 +2,7 @@ package com.example.cryptoapp.data.repository
 
 import android.app.Application
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.Transformations
+import androidx.lifecycle.map
 import com.example.cryptoapp.data.database.AppDatabase
 import com.example.cryptoapp.data.mapper.CoinMapper
 import com.example.cryptoapp.data.network.ApiFactory
@@ -20,7 +20,8 @@ class CoinRepositoryImpl(
     private val mapper = CoinMapper()
 
     override fun getCoinInfoList(): LiveData<List<CoinInfo>> {
-        return Transformations.map(coinInfoDao.getPriceList()) {
+        // Тут ранее было Transformations.map, сейчас заменили на coinInfoDao.getPriceList().map
+        return coinInfoDao.getPriceList().map {
             it.map {
                 mapper.mapDbModelToEntity(it)
             }
@@ -28,7 +29,8 @@ class CoinRepositoryImpl(
     }
 
     override fun getCoinInfo(fromSymbol: String): LiveData<CoinInfo> {
-        return Transformations.map(coinInfoDao.getPriceInfoAboutCoin(fromSymbol)) {
+        // Тут ранее было Transformations.map, сейчас заменили на coinInfoDao.getPriceInfoAboutCoin(fromSymbol).map
+        return coinInfoDao.getPriceInfoAboutCoin(fromSymbol).map {
             mapper.mapDbModelToEntity(it)
         }
     }
