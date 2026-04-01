58063ef0a87e53ccd419016969fe45a182b184f7
LELIK <b_banni@inbox.ru>
2026-04-01 11:30:49 +0300

    Added the application architecture and domain layer

--- app/src/main/java/com/example/cryptoapp/domain/CoinInfo.kt
+++ app/src/main/java/com/example/cryptoapp/domain/CoinInfo.kt
@@ -1,9 +1,5 @@
 package com.example.cryptoapp.domain
 
-import androidx.room.PrimaryKey
-import com.google.gson.annotations.Expose
-import com.google.gson.annotations.SerializedName
-
 data class CoinInfo(
     val fromSymbol: String,
     val toSymbol: String?,
